40fa9fe345285a649b347f92e553400de793c256
Yair LAKRETZ <yair.lakretz@gmail.com>
2022-05-25 22:00:28 +0200

    scatter added

--- analysis/plot_grid_search.py
+++ analysis/plot_grid_search.py
@@ -21,7 +21,7 @@ df = pd.DataFrame()
 for beta, batch_size, latent_size, learning_rate in \
         list(itertools.product(BETAS, BATCH_SIZES,
                                LATENT_SIZES, LEARNING_RATES)):
-    model_name=f'betaB_dletters_beta_{beta}_latent_size_{latent_size}_batch_size_{batch_size}_learning_rate_{learning_rate}'
+    model_name = f'betaB_dletters_beta_{beta}_latent_size_{latent_size}_batch_size_{batch_size}_learning_rate_{learning_rate}'
     fn_log = 'eval.pkl'
     fn_log = os.path.join(path2logs, model_name, fn_log)
     print(f'Loading {fn_log}')
@@ -34,7 +34,9 @@ for beta, batch_size, latent_size, learning_rate in \
                    'batch_size':batch_size,
                    'learning_rate':learning_rate,
                    'recon_loss':losses['recon_loss'],
+                   'neg_recon_loss':-losses['recon_loss'],
                    'MIG':metrics['MIG'],
+                   'log_MIG':np.log10(metrics['MIG']),
                    'model_name':model_name,
                    'model_name_short':f'b_{beta}_ls_{latent_size}_bs_{batch_size}_lr_{learning_rate}'},
                     ignore_index=True)
@@ -52,9 +54,9 @@ print(f'Best model with minimal train loss is: \n {df.iloc[IX_min]}')
 # PLOT
 fig, axs = plt.subplots(2, 2, figsize=(20, 20))
 sns.barplot(data=df, x='beta', y='recon_loss', hue='latent_size', ax=axs[0, 0])
-sns.barplot(data=df, x='beta', y='MIG', hue='latent_size', ax=axs[0, 1])
+sns.barplot(data=df, x='beta', y='log_MIG', hue='latent_size', ax=axs[0, 1])
 sns.barplot(data=df, x='learning_rate', y='recon_loss', hue='batch_size', ax=axs[1, 0])
-sns.barplot(data=df, x='learning_rate', y='MIG', hue='batch_size', ax=axs[1, 1])
+sns.barplot(data=df, x='learning_rate', y='log_MIG', hue='batch_size', ax=axs[1, 1])
 for ax in axs.flatten():
     ax.xaxis.label.set_size(30)
     ax.yaxis.label.set_size(30)
@@ -68,7 +70,7 @@ print(f'Figure saved to: {fn_fig}')
 # PLOT
 fig, axs = plt.subplots(2, 1, figsize=(20, 30))
 sns.barplot(data=df, x='model_name_short', y='recon_loss', ax=axs[0])
-sns.barplot(data=df, x='model_name_short', y='MIG', ax=axs[1])
+sns.barplot(data=df, x='model_name_short', y='log_MIG', ax=axs[1])
 for ax in axs:
     ax.xaxis.label.set_size(30)
     ax.yaxis.label.set_size(30)
@@ -77,6 +79,22 @@ for ax in axs:
     ax.legend().remove()
 plt.subplots_adjust(bottom=0.15, hspace=0.5)
 fn_fig = os.path.join(path2figures, 'grid_search_results_all_models.png')
+fig.savefig(fn_fig)
+plt.close(fig)
+print(f'Figure saved to: {fn_fig}')
+
+# SCATTER
+fig, ax = plt.subplots(1, 1, figsize=(20, 20))
+sns.scatterplot(data=df, x='neg_recon_loss', y='log_MIG')
+ax.xaxis.label.set_size(30)
+ax.yaxis.label.set_size(30)
+ax.tick_params(labelsize=20)
+ax.legend().remove()
+for i, row in df.iterrows():
+    ax.text(row['neg_recon_loss'], row['log_MIG'],
+            row['model_name_short'], fontsize=20)
+plt.subplots_adjust(right=0.85)
+fn_fig = os.path.join(path2figures, 'grid_search_results_scatter.png')
 fig.savefig(fn_fig)
 plt.close(fig)
 print(f'Figure saved to: {fn_fig}')
