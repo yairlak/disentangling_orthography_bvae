a02eff2ac442c65d95c86a43b28db85bf631e37f
yairlak <yair.lakretz@gmail.com>
2022-06-15 10:26:14 +0200

    with bash files

---
+++ bin/train_all.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# RUN every element in the blocks in parallel ! Remove `&` at the end if don't
+# want all in parallel
+
+logger="train_all.out"
+echo "STARTING" > $logger
+for loss in  "betaB" # factor btcvae betaH VAE
+do
+    echo " " >> $logger
+    echo $loss >> $logger
+    for dataset in "dletters" #dsprites celeba chairs mnist
+    do
+        echo $dataset >> $logger
+        cmd="../main.py "$loss"_"$dataset" -x "$loss"_"$dataset"  --no-progress-bar"
+        echo $cmd
+        python $cmd
+    done
+    wait
+done
+
+echo "best_celeba" >> $logger
+python main.py best_celeba -x best_celeba --no-progress-bar &
+
+echo "best_dsprites" >> $logger
+python main.py best_dsprites -x best_dsprites --no-progress-bar
---
+++ bin/train_grid_dLetters.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# RUN every element in the blocks in parallel ! Remove `&` at the end if don't
+# want all in parallel
+
+# SPECS
+loss='betaB'
+dataset='dwords' # dletters/dwords
+
+# HYPERPARAMS
+EPOCHS='2'
+BETAS='1 2 4 8 16'
+BATCH_SIZES='128'
+LATENT_SIZES='4 6 8 16 32 64'
+LEARNING_RATES='0.001 0.0001'
+
+
+# NUMBER OF STIMULI
+for BETA in $BETAS; do
+    for BATCH_SIZE in $BATCH_SIZES; do
+        for LATENT_SIZE in $LATENT_SIZES; do
+            for LEARNING_RATE in $LEARNING_RATES; do
+                name='beta_'$BETA'_latent_size_'$LATENT_SIZE'_batch_size_'$BATCH_SIZE'_learning_rate_'$LEARNING_RATE
+                cmd='python main.py '$loss'_'$dataset'_'$name' --epochs '$EPOCHS' --dataset '$dataset' --experiment custom --eval-batchsize '$BATCH_SIZE' --betaH-B '$BETA' --batch-size '$BATCH_SIZE' --latent-dim '$LATENT_SIZE' --lr '$LEARNING_RATE' --no-progress-bar --is-metrics'
+                echo $cmd
+                eval $cmd
+            done
+        done
+    done
+done
+
+
